3b0c284a7cad03996346094b0ea99c33c2738549
Ihor Baiura <96948641+IhorBaiura@users.noreply.github.com>
2025-04-08 00:14:44 +0200

    feat: add Parentheses client

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ test-unit:
 
 test-unit-coverage:
 	mvn clean test jacoco:report
+
+run-parenthese-client:
+	mvn exec:java -Dexec.mainClass="com.ihorb.clients.Parentheses" -pl client
--- client/src/main/java/com/ihorb/clients/Parentheses.java
+++ client/src/main/java/com/ihorb/clients/Parentheses.java
@@ -1,1 +1,42 @@
 package com.ihorb.clients;
+
+import java.util.Scanner;
+import com.ihorb.datastructures.stack.Stack;
+
+public class Parentheses {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter an expression to check for balanced parentheses:");
+    String input = sc.nextLine();
+
+    Stack<Character> stack = new Stack<>();
+
+    for (int i = 0; i < input.length(); i++) {
+      char c = input.charAt(i);
+
+      if (stack.isEmpty()) {
+        stack.push(c);
+        continue;
+      }
+
+      char current = stack.pop();
+
+      if (c == ')' && current == '('
+          || c == ']' && current == '['
+          || c == '}' && current == '{') {
+        continue;
+      } else {
+        stack.push(current);
+        stack.push(c);
+      }
+    }
+
+    if (stack.isEmpty()) {
+      System.out.println("Balanced parantheses");
+    } else {
+      System.out.println("Unbalaced parantheses");
+    }
+
+    sc.close();
+  }
+}
